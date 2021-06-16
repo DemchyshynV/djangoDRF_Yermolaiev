767d544190c5741e8d067e89d808bd60f4afc28c
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2021-06-16 16:16:15 +0300

    master

--- MAIN_APP/urls.py
+++ MAIN_APP/urls.py
@@ -1,4 +1,4 @@
-"""djangoDRF1 URL Configuration
+"""MAIN_APP URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('cars', include('car.urls'))
 ]

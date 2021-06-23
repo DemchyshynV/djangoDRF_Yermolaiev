6a1351e681b0a7a3acb08f01f87df280db1cf7b3
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2021-06-23 17:17:07 +0300

    master

--- car/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CarListCreateView, CarRetriveUpdeteDeleteView
-
-urlpatterns = [
-    path('', CarListCreateView.as_view(), name='car_list_create'),
-    path('/<int:pk>', CarRetriveUpdeteDeleteView.as_view(), name='car_retrive_update_delete')
-]
